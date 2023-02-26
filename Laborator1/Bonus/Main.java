4cbfcd51025e5e786bdcea8eec57b8a8dc4445db
LilianaPopa-stud <popliliaana@gmail.com>
2023-02-26 15:25:54 +0200

    Update Main.java

--- Laborator1/Bonus/Main.java
+++ Laborator1/Bonus/Main.java
@@ -15,13 +15,39 @@ public class Main {
         System.out.println("Introduceti numarul de noduri ale grafului ciclu: ");
         int n = scanner.nextInt();
 
-        // creeare matrice de adiacenta
+        // creare matrice de adiacenta
         int[][] matrix = new int[n][n];
         Main main = new Main();
         matrix = main.createMatrix(matrix, n);
+
+        //calcularea matricei la puterea k
         main.powerMatrix(matrix, n);
 
+        // nr de noduri ale grafului k-regular si gradul k
+        System.out.println("Introduceti numarul de noduri ale grafului k-regulat: ");
+        int n2 = scanner.nextInt();
+        System.out.println("Introduceti gradul k: ");
+        int k = scanner.nextInt();
+
+        // creare matrice de adiacenta
+        int[][] matrix2 = new int[n2][n2];
+
+        // verific daca exista un graf k-regulat de ordinul n
+        if (n2*k % 2 == 0 && n2>=k+1) // wiki: condițiile necesare și suficiente pentru ca un graf  k-regulat de ordin n sa existe sunt: n>=k+1 si n*k par
+        {
+            matrix2 = main.createMatrixKRegular(matrix2, n2, k);
+        }
+        else
+            System.out.println("Nu exista un graf k-regulat de ordinul n");
+
     }
+
+    private int[][] createMatrixKRegular(int[][] matrix2, int n, int k) {
+        
+
+        return matrix2;
+    }
+
     public int[][] createMatrix(int[][] matrix, int n)
     {
      for (int i=0; i<n; i++){
@@ -42,15 +68,15 @@ public class Main {
         return matrix;
     }
     //calcularea matricei la puterea k
-    public static void powerMatrix(int[][] adiacencyMatrix, int n ){
+    public  void powerMatrix(int[][] adjacencyMatrix, int n ){
 
-        int [][] powerMatrix = Arrays.copyOf(adiacencyMatrix, n);
+        int [][] powerMatrix = Arrays.copyOf(adjacencyMatrix, n);
         for (int power=2; power<=n; power++){
             int [][] auxMatrix = new int[n][n];
             for (int i=0; i<n; i++){
                 for (int j=0; j<n; j++){
                     for (int k=0; k<n; k++){
-                        auxMatrix[i][j] += powerMatrix[i][k] * adiacencyMatrix[k][j];
+                        auxMatrix[i][j] += powerMatrix[i][k] * adjacencyMatrix[k][j];
                     }
                 }
             }
