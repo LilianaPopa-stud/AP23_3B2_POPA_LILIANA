8d31940691de1a70ad42b7333a7d6b17ffb9df03
LilianaPopa-stud <popliliaana@gmail.com>
2023-03-02 19:30:24 +0200

    update compulsory

--- Laborator2/compulsory/Main.java
+++ Laborator2/compulsory/Main.java
@@ -10,28 +10,20 @@ public class Main {
      */
     public static void main(String args[]){
         Location iasi = new Location("Iasi", 23.2, 26.1, LocationType.CITY);
-        Road road = new Road(100, 30, RoadType.CITY);
+        Location cluj = new Location("Cluj", 51.2, 66.1, LocationType.CITY);
+        Road road1 = new Road(50, 50, RoadType.CITY, iasi, cluj );
 
         System.out.println(iasi.toString());
-        System.out.println(road.toString());
-
-        Location cluj = new Location("Cluj", 23.2, 26.1, LocationType.CITY);
-        Road road2 = new Road(100, 50, RoadType.CITY);
-
         System.out.println(cluj.toString());
-        System.out.println(road2.toString());
+        System.out.println(road1.toString());
 
         Location henriCoanda = new Location("Henri Coanda", 23.2, 26.1, LocationType.AIRPORT);
-        Road road3 = new Road(100, 120, RoadType.HIGHWAY);
+        Location hotelTraian = new Location("Hotel Traian", 45.2, 86.1, LocationType.HOTEL);
+        Road road2 = new Road(100, 30, RoadType.CITY,henriCoanda, hotelTraian);
 
         System.out.println(henriCoanda.toString());
-        System.out.println(road3.toString());
-
-        Location hotelTraian = new Location("Hotel Traian", 23.2, 26.1, LocationType.HOTEL);
-        Road road4 = new Road(100, 30, RoadType.CITY);
-
         System.out.println(hotelTraian.toString());
-        System.out.println(road4.toString());
+        System.out.println(road2.toString());
 
         Location museum = new Location();
         museum.setName("Palatul Culturii");
--- Laborator2/compulsory/Road.java
+++ Laborator2/compulsory/Road.java
@@ -1,4 +1,8 @@
 package compulsory;
+
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+
 /**
  * This class represents a road
  * @author Popa Liliana
@@ -7,20 +11,22 @@ public class Road {
     private double length;
     private int speedLimit;
     private  RoadType type;
-    
+    Location c1;
+    Location c2;
     public Road() {
     }
 
-    /**
-     * This is the constructor of the class
-     * @param length This is the length of the road
-     * @param speedLimit This is the speed limit of the road
-     * @param type This is the type of the road
-     */
-    public Road(float length, int speedLimit, RoadType type) {
-        this.length = length;
-        this.speedLimit = speedLimit;
-        this.type = type;
+    public Road(double length, int speedLimit, RoadType type, Location c1, Location c2) {
+        if (length < sqrt(pow(c1.getX() - c2.getX(), 2) + pow(c1.getY() - c2.getY(), 2))) {
+            throw new IllegalArgumentException("The length of the road is smaller than the distance between the two locations");
+        }
+        else {
+            this.length = length;
+            this.speedLimit = speedLimit;
+            this.type = type;
+            this.c1 = c1;
+            this.c2 = c2;
+        }
     }
 
     public double getLength() {
@@ -47,12 +53,30 @@ public class Road {
         this.type = type;
     }
 
+    public Location getC1() {
+        return c1;
+    }
+
+    public void setC1(Location c1) {
+        this.c1 = c1;
+    }
+
+    public Location getC2() {
+        return c2;
+    }
+
+    public void setC2(Location c2) {
+        this.c2 = c2;
+    }
+
     @Override
     public String toString() {
         return "Road{" +
                 "length=" + length +
                 ", speedLimit=" + speedLimit +
                 ", type=" + type +
+                ", C1=" + c1 +
+                ", C2=" + c2 +
                 '}';
     }
 }
